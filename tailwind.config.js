ea5da77229efeacda93749bec9b3dc474e256e2f
jnylen <me@jnylen.nu>
2019-09-05 14:45:19 +0200

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,187 @@
+module.exports = {
+  prefix: "",
+  important: false,
+  separator: ":",
+  theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px"
+    },
+    colors: {
+      transparent: "transparent",
+
+      black: "#000",
+      white: "#fff",
+      purple: {
+        light: "#667EEA",
+        dark: "#3C366B"
+      },
+      blue: {
+        light: "#C3DAFE"
+      }
+    },
+    spacing: {
+      px: "1px",
+      "0": "0",
+      "1": "0.25rem",
+      "2": "0.5rem",
+      "3": "0.75rem",
+      "4": "1rem",
+      "5": "1.25rem",
+      "6": "1.5rem",
+      "8": "2rem",
+      "10": "2.5rem",
+      "12": "3rem",
+      "16": "4rem",
+      "20": "5rem",
+      "24": "6rem",
+      "32": "8rem",
+      "40": "10rem",
+      "48": "12rem",
+      "56": "14rem",
+      "64": "16rem"
+    },
+    backgroundColor: theme => theme("colors"),
+    backgroundSize: {
+      auto: "auto",
+      cover: "cover",
+      contain: "contain"
+    },
+    borderRadius: {
+      none: "0",
+      sm: "0.125rem",
+      default: "0.25rem",
+      lg: "0.5rem",
+      full: "9999px"
+    },
+    boxShadow: {
+      default:
+        "0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)",
+      md:
+        "0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
+      lg:
+        "0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
+      xl:
+        "0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)",
+      "2xl": "0 25px 50px -12px rgba(0, 0, 0, 0.25)",
+      inner: "inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)",
+      outline: "0 0 0 3px rgba(66, 153, 225, 0.5)",
+      none: "none"
+    },
+    flex: {
+      "1": "1 1 0%",
+      auto: "1 1 auto",
+      initial: "0 1 auto",
+      none: "none"
+    },
+    flexGrow: {
+      "0": "0",
+      default: "1"
+    },
+    flexShrink: {
+      "0": "0",
+      default: "1"
+    },
+    fontFamily: {
+      sans: [
+        "Nunito",
+        "BlinkMacSystemFont",
+        "-apple-system",
+        '"Helvetica Neue"'
+      ]
+    },
+    fontSize: {
+      xs: "0.75rem",
+      sm: "0.875rem",
+      base: "1rem",
+      lg: "1.125rem",
+      xl: "1.25rem",
+      "2xl": "1.5rem",
+      "3xl": "1.875rem",
+      "4xl": "2.25rem",
+      "5xl": "3rem",
+      "6xl": "4rem"
+    },
+    fontWeight: {
+      hairline: "100",
+      thin: "200",
+      light: "300",
+      normal: "400",
+      medium: "500",
+      semibold: "600",
+      bold: "700",
+      extrabold: "800",
+      black: "900"
+    },
+    stroke: {
+      current: 'currentColor',
+    },
+    height: theme => ({
+      auto: "auto",
+      ...theme("spacing"),
+      full: "100%",
+      screen: "100vh"
+    }),
+    padding: theme => theme("spacing"),
+    width: theme => ({
+      auto: "auto",
+      ...theme("spacing"),
+      "1/2": "50%",
+      "1/3": "33.333333%",
+      "2/3": "66.666667%",
+      "1/4": "25%",
+      "2/4": "50%",
+      "3/4": "75%",
+      "1/5": "20%",
+      "2/5": "40%",
+      "3/5": "60%",
+      "4/5": "80%",
+      "1/6": "16.666667%",
+      "2/6": "33.333333%",
+      "3/6": "50%",
+      "4/6": "66.666667%",
+      "5/6": "83.333333%",
+      "1/12": "8.333333%",
+      "2/12": "16.666667%",
+      "3/12": "25%",
+      "4/12": "33.333333%",
+      "5/12": "41.666667%",
+      "6/12": "50%",
+      "7/12": "58.333333%",
+      "8/12": "66.666667%",
+      "9/12": "75%",
+      "10/12": "83.333333%",
+      "11/12": "91.666667%",
+      full: "100%",
+      screen: "100vw"
+    }),
+    textColor: theme => theme("colors"),
+    extend: {}
+  },
+  variants: {
+    alignContent: ["responsive"],
+    alignItems: ["responsive"],
+    alignSelf: ["responsive"],
+    backgroundColor: ["responsive", "hover", "focus"],
+    boxShadow: ["responsive", "hover", "focus"],
+    display: ["responsive"],
+    flex: ["responsive"],
+    flexDirection: ["responsive"],
+    flexGrow: ["responsive"],
+    flexShrink: ["responsive"],
+    flexWrap: ["responsive"],
+    float: ["responsive"],
+    stroke: ['responsive'],
+    fontFamily: ["responsive"],
+    fontSize: ["responsive"],
+    fontSmoothing: ["responsive"],
+    fontStyle: ["responsive"],
+    fontWeight: ["responsive", "hover", "focus"],
+    justifyContent: ["responsive"],
+    padding: ["responsive"],
+    width: ["responsive"]
+  },
+  plugins: []
+};
